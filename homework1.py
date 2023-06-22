2276d09f882d17f2376873df0ffa49007be690d6
Артем Щукин <a_r_shukin@mail.ru>
2023-06-22 11:02:09 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,7 @@
+print('Введите вид питомца')
+type = input()
+print('Введите кличку питомца')
+nickname = input()
+print('Введите возраст питомца')
+age = input()
+print('Это ' + type + ' по кличке ' + nickname + '. Возраст: ' + age + ' года.')
